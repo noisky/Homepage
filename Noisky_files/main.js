bcf6247f1758cc8c73908912efc6c04cac721c77
饭饭 <76698242@qq.com>
2019-08-28 22:22:07 +0800

    fix #2，将一言和日期js合并到js文件中，修复了移动端菜单栏bug，优化冗余
    再次感谢大佬 @woytu

--- Noisky_files/main.js
+++ Noisky_files/main.js
@@ -1,86 +1,58 @@
 /**
  * Created by Noisky on 17/05/13.
- * Revised by Noisky on 19/08/11.
+ * Revised by Noisky on 19/08/28.
  */
 $(document).ready(function() {
+    var nw = $('.navigation-wrapper');
+    // 移动端下拉菜单栏
+    function bounceOutUp() {
 
-    $('a.blog-button').click(function() {
-        if (location.hash && location.hash == "#blog") return;
-        if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
-        $('.main-post-list').removeClass('hidden');
-        currentWidth = $('.panel-cover').width();
-        if (currentWidth < 960) {
-            $('.panel-cover').addClass('panel-cover--collapsed');
-        } else {
-            $('.panel-cover').css('max-width', currentWidth);
-            $('.panel-cover').animate({ 'max-width': '700px', 'width': '30%' }, 400, swing = 'swing', function() {});
-        }
-    });
-
-    if (window.location.hash && window.location.hash == "#blog") {
-        $('.panel-cover').addClass('panel-cover--collapsed');
-        $('.main-post-list').removeClass('hidden');
-    }
-
-    if (window.location.pathname.substring(0, 5) == "/tag/") {
-        $('.panel-cover').addClass('panel-cover--collapsed');
+        nw.on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            nw.toggleClass('visible animated bounceOutUp');
+            nw.off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+        });
+        nw.toggleClass('animated bounceInDown animated bounceOutUp');
     }
 
     $('.btn-mobile-menu').click(function() {
-        if ($('.navigation-wrapper').css('display') == "block") {
-            $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-                $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-            });
-            $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+        if (nw.hasClass("bounceInDown")) {
+            bounceOutUp();
         } else {
-            $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+            nw.toggleClass('visible animated bounceInDown');
         }
-        $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
+        $('.btn-mobile-menu_icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
     });
-
-    // 判断是否为移动端
-    var isMobile = navigator.userAgent.toLowerCase().match(/(ipod|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;
 
     // 点击下拉菜单以外的其他标签区域收起菜单生效
-    $(document).on('click', ':not(.mobile,.navigation-wrapper,.btn-mobile-menu,.btn-mobile-menu__icon,panel-main__content)', function(e) {
-        if ($('.navigation-wrapper').css('display') == "block" && isMobile) {
-            $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-                $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-            });
-            $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
-            $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
-        }
-    });
-
-    // 点击下拉菜单以外的空白区域收起菜单生效
-    $(".panel-cover").on('click', ':not(.navigation-wrapper)', function(e) {
-        if ($('.navigation-wrapper').css('display') == "block" && isMobile) {
-            $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-                $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-            });
-            $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
-            $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
+    $(".panel-main").on('click', ':not(.mobile,.btn-mobile-menu,.navigation-wrapper)', function() {
+        if (nw.hasClass("bounceInDown")) {
+            bounceOutUp();
+            $('.btn-mobile-menu_icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
         }
     });
     // 阻止冒泡事件执行
-    $(".navigation-wrapper").click(function(event) {
+    nw.click(function(event) {
         event.stopPropagation();
     });
 
-    $('.navigation-wrapper .blog-button').click(function() {
-        if ($('.navigation-wrapper').css('display') == "block") {
-            $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-                $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-            });
-
-            $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
-        }
-
-        $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
-    });
+    // 底部年份动态化
     $('.year').html(new Date().getFullYear());
 });
+
+// 一言异步加载代码
+function getHitokoto() {
+    $.ajax({
+        url: "https://api.imjad.cn/hitokoto/?encode=jsc&charset=utf-8&length=50",
+        dataType: "jsonp",
+        async: true,
+        jsonp: "callback",
+        jsonpCallback: "hitokoto",
+        success: function(result) {
+            $('#hitokoto').html("<p>" + result.hitokoto + "</p>")
+        },
+        error: function() {
+            $('#hitokoto').html("<p>读取数据失败了的说……_(:з」∠)_</p>")
+        }
+    });
+}
+getHitokoto();
